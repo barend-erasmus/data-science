631e92ab49efcb77aa68c5761cae46e4f7c9c649
Barend Erasmus <developersworkspace@gmail.com>
2018-07-26 16:29:31 +0300

    update

--- src/heatmap.js
+++ src/heatmap.js
@@ -1,29 +1,24 @@
 const colors = require("./colors");
 const fs = require("fs").promises;
 const parser = require("./parser");
-const statsLite = require('stats-lite');
+const statsLite = require("stats-lite");
 const { SVGElement } = require("./svg-element");
 
 (async () => {
-  let data = await parser.parseCSV(
-    "tomslee_airbnb_london_0250_2015-12-25.csv"
-  );
-
-  data = data.filter((x) => parseFloat(x.price) < 300);
+  let data = await parser.parseCSV("tomslee_airbnb_london_1486_2017-07-28.csv");
 
   const matrix = [];
 
   let maximiumX = 0;
   let maximiumY = 0;
-  let maximiumValue = 0;
 
   for (const datum of data) {
-    const x = parseFloat(datum.minstay);
+    const x = parseFloat(datum.accommodates);
     const y = parseFloat(datum.bedrooms);
     const value = parseFloat(datum.price);
 
     if (!x || !y || !value) {
-        continue;
+      continue;
     }
 
     if (!matrix[x]) {
@@ -43,21 +38,15 @@ const { SVGElement } = require("./svg-element");
     if (y > maximiumY) {
       maximiumY = y;
     }
-
-    if (value > maximiumValue) {
-        maximiumValue = value;
-    }
   }
 
   const heightPerItem = 30;
   const widthPerItem = 30;
 
-  const height = (maximiumY * heightPerItem) + heightPerItem;
-  const width = (maximiumX * widthPerItem) + widthPerItem;
+  const height = maximiumY * heightPerItem + heightPerItem;
+  const width = maximiumX * widthPerItem + widthPerItem;
 
-  const svgElement = SVGElement.create("svg")
-    .attr("height", height)
-    .attr("width", width);
+  const heatmapMatrix = [];
 
   for (let x = 0; x < matrix.length; x++) {
     if (!matrix[x]) {
@@ -71,23 +60,38 @@ const { SVGElement } = require("./svg-element");
 
       const values = matrix[x][y];
 
-      const value = values.sort()[values.length - 1];
+      const value = statsLite.median(values);
 
-      const percentage = value / maximiumValue;
-
-      const color = colors.calculateGradient([255, 0, 0], [0, 0, 255], percentage);
-
-      svgElement
-        .append("rect")
-        .attr("x", x * widthPerItem)
-        .attr("y", y * heightPerItem)
-        .attr("height", heightPerItem)
-        .attr("width", widthPerItem)
-        .attr(
-          "fill",
-          `rgb(${color.join(',')})` 
-        );
+      heatmapMatrix.push([x, y, value]);
     }
+  }
+
+  let maximiumValue = Math.max(...heatmapMatrix.map(x => x[2]));
+
+  const svgElement = SVGElement.create("svg")
+    .attr("height", height)
+    .attr("width", width);
+
+  for (const item of heatmapMatrix) {
+    const x = item[0];
+    const y = item[1];
+    const value = item[2];
+
+    const percentage = value / maximiumValue;
+
+    const color = colors.calculateGradient(
+      [255, 0, 0],
+      [0, 0, 255],
+      percentage
+    );
+
+    svgElement
+      .append("rect")
+      .attr("x", x * widthPerItem)
+      .attr("y", y * heightPerItem)
+      .attr("height", heightPerItem)
+      .attr("width", widthPerItem)
+      .attr("fill", `rgb(${color.join(",")})`);
   }
 
   await fs.writeFile("test.svg", svgElement.toString());
