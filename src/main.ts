d5ba8b418f1df3d89e1c74106e98fd9460133a31
Barend Erasmus <developersworkspace@gmail.com>
2018-07-29 20:14:06 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as fs from 'fs';
 import { ScatterPlot } from './visualizations/scatter-plot';
 import * as sass from 'node-sass';
-import { Parser } from './parser';
-import { EuclideanDistance } from './statistics/euclidean-distance';
 
 const theme: string = sass
   .renderSync({
@@ -11,29 +9,17 @@ const theme: string = sass
   .css.toString();
 
 (async () => {
-  const parser: Parser = new Parser('wind-guru-diving-visibility.csv');
+  const xAxis: number[] = Array.from({
+    length: 30,
+  }).map(() => Math.random() * 100);
 
-  const data: any[] = await parser.fromCSV();
-
-  const xAxis: number[] = data.map((x: any) => parseFloat(x['visibility']));
-
-  const yAxis: Array<number> = data.map((x: any) => {
-    return new EuclideanDistance(
-      [
-        // x['temperatureInCelsius'],
-        x['waveDirectionInDegrees'],
-        x['waveHeightInMeters'],
-        x['wavePeriodInSeconds'],
-        x['windDirectionInDegrees'],
-        x['windSpeedInKnots'],
-      ],
-      [0, 0, 0, 0, 0],
-    ).calculate();
-  });
+  const yAxis: Array<number> = Array.from({
+    length: 30,
+  }).map(() => Math.random() * 100);
 
   const scatterPlot: ScatterPlot = new ScatterPlot(600, 600);
 
-  scatterPlot.setData([xAxis], [yAxis]);
+  scatterPlot.setData([xAxis], [yAxis]).setMinimumAndMaximum(0, 100, 0, 100);
 
   const svg = scatterPlot.toString();
 
